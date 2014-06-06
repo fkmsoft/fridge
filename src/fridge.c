1850ee0f88ccea3f9c1a09bd75ae99598bca072b
Richard Molitor <gattschardo@gmail.com>
2014-06-06 15:29:19 +0200

    configurable animations displayed in game

--- src/fridge.c
+++ src/fridge.c
@@ -17,17 +17,9 @@ struct session {
 	int h;
 };
 
-struct game_rules {
-	int start_x;
-	int start_y;
-	int walk_dist;
-	int jump_dist;
-	int jump_time;
-	int fall_dist;
-};
-
 enum dir { DIR_LEFT, DIR_RIGHT };
 enum state { ST_IDLE, ST_WALK, ST_FALL, ST_JUMP, NSTATES };
+char const * const st_names[] = { "idle", "walk", "fall", "jump" };
 
 struct animation_rule {
 	unsigned len;
@@ -37,11 +29,24 @@ struct animation_rule {
 };
 
 struct entity_rule {
-	struct {
-		int x;
-		int y;
-	} start;
+	SDL_Rect start_dim;
 	struct animation_rule anim[NSTATES];
+};
+
+struct game_rules {
+	int start_x;
+	int start_y;
+	int walk_dist;
+	int jump_dist;
+	int jump_time;
+	int fall_dist;
+	struct entity_rule player;
+};
+
+struct animation_state {
+		int pos;
+		int frame;
+		int remaining;
 };
 
 struct entity_state {
@@ -49,10 +54,7 @@ struct entity_state {
 	enum dir dir;
 	enum state st;
 	int jump_timeout;
-	struct {
-		int frame;
-		int remaining;
-	} anim;
+	struct animation_state anim;
 };
 
 struct game_state {
@@ -85,12 +87,14 @@ SDL_bool init_game(session *s, game_rules *r, game_state *g, char const *root);
 void process_event(SDL_Event const *ev, game_event *r);
 void process_keystate(unsigned char const *ks, game_event *r);
 void update_gamestate(session const *s, game_rules const *gr, game_state *gs, game_event const *ev);
+void tick_animation(struct animation_state *as, struct animation_rule const *ar);
+void load_state(struct entity_state *es, struct animation_rule const *ar);
 void render(session const *s, game_state const *gs);
 void clear_event(game_event *ev);
 
 SDL_bool collides_with_terrain(SDL_Rect const *r, SDL_Surface const *t);
 SDL_bool stands_on_terrain(SDL_Rect const *r, SDL_Surface const *t);
-void player_rect(int x, int y, SDL_Rect *r);
+void player_rect(SDL_Rect const *pos, SDL_Rect *r);
 
 /* low level interactions */
 char const *get_asset(json_t *a, char const *k);
@@ -98,6 +102,8 @@ SDL_bool get_int_field(json_t *o, char const *s, int *r);
 SDL_Texture *load_texture(SDL_Renderer *r, char const *file);
 SDL_Texture *load_asset_tex(json_t *a, char const *d, SDL_Renderer *r, char const *k);
 SDL_Surface *load_asset_surf(json_t *a, char const *d, char const *k);
+SDL_bool load_entity_resource(json_t *src, SDL_Texture **t, SDL_Renderer *r, struct entity_rule *er, char const *root);
+void load_anim(json_t *src, char const *key, struct animation_rule *a);
 void draw_background(SDL_Renderer *r, SDL_Texture *bg, int x, int y);
 void draw_player(SDL_Renderer *r, SDL_Texture *pl, unsigned int frame, SDL_bool flip);
 unsigned getpixel(SDL_Surface const *s, int x, int y);
@@ -197,7 +203,7 @@ SDL_bool init_session(session *s, char const *root)
 	return SDL_TRUE;
 }
 
-SDL_bool init_game(session *s, game_rules *r, game_state *g, char const *root)
+SDL_bool init_game(session *s, game_rules *gr, game_state *gs, char const *root)
 {
 	json_t *game, *player, *start;
 
@@ -217,44 +223,35 @@ SDL_bool init_game(session *s, game_rules *r, game_state *g, char const *root)
 		return SDL_FALSE;
 	}
 
-	r->start_x = json_integer_value(json_array_get(start, 0));
-	r->start_y = json_integer_value(json_array_get(start, 1));
+	gr->start_x = json_integer_value(json_array_get(start, 0));
+	gr->start_y = json_integer_value(json_array_get(start, 1));
 
 	SDL_bool ok;
-	ok = get_int_field(player, "walk_dist", &r->walk_dist);
+	ok = get_int_field(player, "walk_dist", &gr->walk_dist);
 	if (!ok) { return SDL_FALSE; }
-	ok = get_int_field(player, "jump_dist", &r->jump_dist);
+	ok = get_int_field(player, "jump_dist", &gr->jump_dist);
 	if (!ok) { return SDL_FALSE; }
-	ok = get_int_field(player, "jump_time", &r->jump_time);
+	ok = get_int_field(player, "jump_time", &gr->jump_time);
 	if (!ok) { return SDL_FALSE; }
-	ok = get_int_field(player, "fall_dist", &r->fall_dist);
+	ok = get_int_field(player, "fall_dist", &gr->fall_dist);
 	if (!ok) { return SDL_FALSE; }
 
-	json_t *po;
-	char const *ps;
-	po = json_object_get(player, "resource");
-	ps = json_string_value(po);
-	sprintf(buf, "%s/%s/%s", root, "conf", ps);
-
-	json_error_t e;
-	po = json_load_file(buf, 0, &e);
-
-	sprintf(buf, "%s/%s", root, "assets");
-	s->player = load_asset_tex(po, buf, s->r, "asset");
-	if (!s->player) { return SDL_FALSE; }
-
-	json_decref(po);
+        ok = load_entity_resource(player, &s->player, s->r, &gr->player, root);
+	if (!ok) {
+		fprintf(stderr, "no player resource\n");
+		return SDL_FALSE;
+	}
 
 	json_decref(game);
 
-	g->player.dir = DIR_LEFT;
-	g->player.st = ST_IDLE;
-	g->player.pos.x = r->start_x;
-	g->player.pos.y = r->start_y;
-	g->run = SDL_TRUE;
-	g->player.anim.frame = 0;
-	g->player.anim.remaining = 1;
-	g->player.jump_timeout = 0;
+	gs->player.dir = DIR_LEFT;
+	gs->player.st = ST_IDLE;
+	gs->player.pos.x = gr->start_x;
+	gs->player.pos.y = gr->start_y;
+	gs->run = SDL_TRUE;
+	gs->player.anim.frame = 0;
+	gs->player.anim.remaining = 1;
+	gs->player.jump_timeout = 0;
 
 	return SDL_TRUE;
 }
@@ -293,33 +290,28 @@ void process_keystate(unsigned char const *ks, game_event *r)
 
 void update_gamestate(session const *s, game_rules const *gr, game_state *gs, game_event const *ev)
 {
-	gs->player.anim.frame = (gs->player.anim.frame + 1) % 2;
-
-	gs->player.st = gs->player.jump_timeout > 0 ? ST_FALL : ST_IDLE;
-
-	if (ev->player.move_left || ev->player.move_right) {
-		gs->player.st = ST_WALK;
-		gs->player.dir = ev->player.move_left ? DIR_LEFT : DIR_RIGHT;
-	}
-
-	int old_x = gs->player.pos.x;
-	int dir = (gs->player.dir == DIR_LEFT) ? -1 : 1;
-	if (gs->player.st == ST_WALK) {
-		gs->player.pos.x += dir * gr->walk_dist;
-	}
+	tick_animation(&gs->player.anim, &gr->player.anim[gs->player.st]);
 
 	SDL_Rect r;
-	player_rect(gs->player.pos.x, gs->player.pos.y, &r);
+	if (ev->player.move_left || ev->player.move_right) {
+		gs->player.dir = ev->player.move_left ? DIR_LEFT : DIR_RIGHT;
 
-	if (collides_with_terrain(&r, s->collision)) {
-		gs->player.pos.x = old_x;
-		player_rect(gs->player.pos.x, gs->player.pos.y, &r);
-		/*
-		do {
-			gs->player_x += dir;
-			r->x += dir;
-		} while (!collides_with_terrain(&r, s->collision));
-		*/
+		int old_x = gs->player.pos.x;
+		int dir = (gs->player.dir == DIR_LEFT) ? -1 : 1;
+		gs->player.pos.x += dir * gr->walk_dist;
+
+		player_rect(&gs->player.pos, &r);
+
+		if (collides_with_terrain(&r, s->collision)) {
+			gs->player.pos.x = old_x;
+			player_rect(&gs->player.pos, &r);
+			/*
+			do {
+				gs->player_x += dir;
+				r->x += dir;
+			} while (!collides_with_terrain(&r, s->collision));
+			*/
+		}
 	}
 
 	if (ev->player.move_jump && gs->player.st != ST_FALL) {
@@ -328,25 +320,43 @@ void update_gamestate(session const *s, game_rules const *gr, game_state *gs, ga
 
 	if (gs->player.jump_timeout > 0) {
 		int old_y = gs->player.pos.y;
-		gs->player.st = ST_FALL;
 		gs->player.jump_timeout -= 1;
 		gs->player.pos.y -= gr->jump_dist;
-		gs->player.anim.frame = 1;
 
-		player_rect(gs->player.pos.x, gs->player.pos.y, &r);
+		player_rect(&gs->player.pos, &r);
 		if (collides_with_terrain(&r, s->collision)) {
 			gs->player.pos.y = old_y;
 			gs->player.jump_timeout = 0;
 		}
 	} else {
-		player_rect(gs->player.pos.x, gs->player.pos.y, &r);
+		player_rect(&gs->player.pos, &r);
 		int f;
 		for (f = gr->jump_dist; !stands_on_terrain(&r, s->collision) && f > 0; f--) {
-			gs->player.st = ST_FALL;
-			gs->player.anim.frame = 0;
 			r.y += 1;
 			gs->player.pos.y += 1;
 		}
+	}
+
+	enum state old_state = gs->player.st;
+	player_rect(&gs->player.pos, &r);
+	if (!stands_on_terrain(&r, s->collision)) {
+
+		gs->player.st = ST_FALL;
+
+	} else if (gs->player.jump_timeout > 0) {
+
+		gs->player.st = ST_JUMP;
+
+	} else if (ev->player.move_left || ev->player.move_right) {
+
+		gs->player.st = ST_WALK;
+	} else {
+
+		gs->player.st = ST_IDLE;
+	}
+
+	if (old_state != gs->player.st) {
+		load_state(&gs->player, &gr->player.anim[gs->player.st]);
 	}
 
 	if (ev->reset) {
@@ -359,12 +369,31 @@ void update_gamestate(session const *s, game_rules const *gr, game_state *gs, ga
 	}
 }
 
+void tick_animation(struct animation_state *as, struct animation_rule const *ar)
+{
+	as->remaining -= 1;
+	if (as->remaining < 0) {
+		as->pos = (as->pos + 1) % ar->len;
+		as->frame = ar->frames[as->pos];
+		as->remaining = ar->duration[as->pos];
+	}
+}
+
+void load_state(struct entity_state *es, struct animation_rule const *ar)
+{
+	es->anim.pos = 0;
+	es->anim.frame = ar->frames[0];
+	es->anim.remaining = ar->duration[0];
+	es->pos.w = ar->box.w;
+	es->pos.h = ar->box.h;
+}
+
 void render(session const *s, game_state const *gs)
 {
 	SDL_RenderClear(s->r);
 
 	draw_background(s->r, s->background, gs->player.pos.x, gs->player.pos.y);
-	draw_player(s->r, s->player, gs->player.st * 2 + gs->player.anim.frame, gs->player.dir == DIR_RIGHT);
+	draw_player(s->r, s->player, gs->player.anim.frame, gs->player.dir == DIR_RIGHT);
 
 	SDL_RenderPresent(s->r);
 }
@@ -403,12 +432,12 @@ SDL_bool stands_on_terrain(SDL_Rect const *r, SDL_Surface const *t)
 	return getpixel(t, x, r->y + r->h + 1) == 0;
 }
 
-void player_rect(int x, int y, SDL_Rect *r)
+void player_rect(SDL_Rect const *pos, SDL_Rect *r)
 {
-	r->x = x + 400 - 9;
-	r->y = y + 300 - 25;
-	r->w = 18;
-	r->h = 55;
+	r->x = pos->x + 400 - 9;
+	r->y = pos->y + 300 - 25;
+	r->w = /*pos->w*/ 18;
+	r->h = /*pos->h*/ 55;
 }
 
 /* low level interactions */
@@ -497,6 +526,67 @@ SDL_Surface *load_asset_surf(json_t *a, char const *d, char const *k)
 	sprintf(p, "%s/%s", d, f);
 
 	return IMG_Load(p);
+}
+
+SDL_bool load_entity_resource(json_t *src, SDL_Texture **t, SDL_Renderer *r, struct entity_rule *er, char const *root)
+{
+	json_t *o;
+	char const *ps;
+	o = json_object_get(src, "resource");
+	ps = json_string_value(o);
+	char buf[500];
+	sprintf(buf, "%s/%s/%s", root, "conf", ps);
+
+	json_error_t e;
+	o = json_load_file(buf, 0, &e);
+
+	sprintf(buf, "%s/%s", root, "assets");
+	*t = load_asset_tex(o, buf, r, "asset");
+	if (!t) { return SDL_FALSE; }
+
+	int i;
+	for (i = 0; i < NSTATES; i++) {
+		load_anim(o, st_names[i], &er->anim[i]);
+	}
+
+	json_decref(o);
+
+	return SDL_TRUE;
+}
+
+void load_anim(json_t *src, char const *key, struct animation_rule *a)
+{
+	json_t *o, *frames, *dur, *box;
+	o = json_object_get(src, key);
+	if (!o) {
+		fprintf(stderr, "warning: no %s animation\n", key);
+		return;
+	}
+
+	frames = json_object_get(o, "frames");
+	dur = json_object_get(o, "duration");
+
+	int k, l;
+	l = json_array_size(frames);
+	k = json_array_size(dur);
+	if (k != l) {
+		fprintf(stderr, "error: have %d frames but %d durations\n", l, k);
+		return;
+	}
+	a->len = l;
+	a->frames   = malloc(sizeof(unsigned) * l);
+	a->duration = malloc(sizeof(unsigned) * l);
+	int i;
+	for (i = 0; i < l; i++) {
+		a->frames[i] = json_integer_value(json_array_get(frames, i));
+		a->duration[i] = json_integer_value(json_array_get(dur, i));
+	}
+
+	box = json_object_get(o, "box");
+	a->box.x = json_integer_value(json_array_get(box, 0));
+	a->box.y = json_integer_value(json_array_get(box, 1));
+	a->box.w = json_integer_value(json_array_get(box, 2));
+	a->box.h = json_integer_value(json_array_get(box, 3));
 }
 
 unsigned getpixel(SDL_Surface const *s, int x, int y)
